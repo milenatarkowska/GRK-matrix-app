0a0749f3afcda3bde0b52b3583df8113db263c66
milenatarkowska <milenatarkowska1@gmail.com>
2025-04-25 12:01:47 +0200

    cleared files from unused code, added methods updateMatrixDisplay and resetMatrix

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-var imgA;
-var imgB;
+let imgA;
+let imgB;
+let currentMatrix;
 
 function setup() {
-    createCanvas(512,512);
+    createCanvas(1024,512);
     background(255);
+
     imgA = createImage(512,512);
     imgB = createImage(512,512);
     imgA.loadPixels();
     imgB.loadPixels();
-    var d = pixelDensity();
-    for(var i=0; i<512*512*4*d; i+=4) {
+
+    let d = pixelDensity();
+
+    for(let i=0; i<512*512*4*d; i+=4) {
         imgA.pixels[i]=240;
         imgA.pixels[i+1]=250;
         imgA.pixels[i+2]=240;
@@ -19,9 +23,11 @@ function setup() {
         imgB.pixels[i+2]=250;
         imgB.pixels[i+3]=255;
     }
+
     imgA.updatePixels();
     imgB.updatePixels();
 }
+
 function draw() {
     if (!keyIsDown(32)) {
         image(imgA,0,0);
@@ -31,7 +37,15 @@ function draw() {
         text('Image B',10,20);
     }
 }
-// zadanie 1
+
+function mouseDragged(){
+    let vec = makeVector(mouseX, mouseY);
+    drawVector(imgA, vec);
+    drawVector(imgB, transformMatrix(currentMatrix, vec));
+}
+
+// WEKTORY I MACIERZE
+
 // tworzy wektor w 2d
 function makeVector(x, y){
     return [x, y, 1];
@@ -43,20 +57,6 @@ function drawVector(img, vec){
     img.set(vec[0], vec[1], 0);
     img.updatePixels();
 }
-
-function mouseDragged(){
-    let vec = makeVector(mouseX, mouseY);
-    drawVector(imgA, vec);
-    //drawVector(imgB, transformMatrix(makeIdentity(), vec));
-    //drawVector(imgB, transformMatrix(makeShift(0, -25), vec));
-    //drawVector(imgB, transformMatrix(makeScale(0.5, 0.5), vec));
-    //drawVector(imgB, transformMatrix(makeRotate(0.25 * Math.PI), vec));
-    //drawVector(imgB, transformMatrix(makeShear(0.4, 0.2), vec));
-    drawVector(imgB, transformMatrix(multiplyMatrix(makeShift(20, 30), makeScale(0.7, 0.3)), vec));
-
-}
-
-//funkcje tworzące macierze - zadanie 2
 
 //macierz jednostkowa
 function makeIdentity() {
@@ -83,7 +83,6 @@ function makeShear(shx, shy) {
     return [[1, shx, 0], [shy, 1, 0], [0, 0, 1]];
 }
 
-//zadanie 3 - transformacja (mnożenie) macierzy
 function transformMatrix(matrix, vector){
     let value = [0,0,0];
     for(let i = 0; i < 3 ; ++i){
@@ -94,8 +93,6 @@ function transformMatrix(matrix, vector){
     return value;
 }
 
-//zadanie 4 - łączenie przekształceń
-
 function multiplyMatrix(matrix1, matrix2){
     value = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     for (i = 0; i < 3; ++i) {
@@ -104,4 +101,18 @@ function multiplyMatrix(matrix1, matrix2){
         }
     }
     return value;
+}
+
+// FUNKCJE DO INTERAKCJI Z UI
+
+function updateMatrixDisplay(){
+    display = currentMatrix.map(row =>
+        row.map(val => val.toFixed(2).padStart(6)).join(" ")
+    ).join("\n");
+    document.getElementById("matrixDisplay").innerText = display;
+}
+
+function resetMatrix(){
+    currentMatrix = makeIdentity();
+    updateMatrixDisplay();
 }
